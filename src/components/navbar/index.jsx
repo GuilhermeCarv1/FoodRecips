be105f1602b57d114ce209e879fa2e89471ea65b
guilherme <guicarvalhooliveira2468@gmail.com>
2024-02-22 20:53:21 +0000

    pages

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,15 +1,24 @@
-import { NavLink } from 'react-router-dom';
+import { useContext } from "react";
+import { NavLink } from "react-router-dom";
+import { GlobalContext } from "../../context";
 
 export default function Navbar() {
+  const { searchParam, setSearchParam , handleSubmit } = useContext(GlobalContext);
+  
+  console.log(searchParam);
+
   return (
     <nav className="flex justify-between gray-100 items-center py-8 container mx-auto flex-col lg:flex-row gap-5 lg:gap-0">
       <h2 className="text-2xl font-semibold">
         <NavLink to={"/"}>FoodRecipe</NavLink>
       </h2>
-      <form>
+      
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           name="search"
+          value={searchParam}
+          onChange={(event) => setSearchParam(event.target.value)}
           placeholder="Digite os itens..."
           className="bg-white/75 p-3 px-8 rounded-full outline-none lg:w-96 shadow-lg shadow-red-100 focus"
         />
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,18 @@
-
+import { useContext } from "react"
+import { GlobalContext } from "../../context";
+import RecipeItem from "../../components/recipe-item";
 
 export default function Home() {
   
+  const {recipeList, loading} = useContext(GlobalContext);
 
   return (
-    <div >
-     Home
+    <div className="py-8 container mx-auto flex flex-wrap justify-center gap-10">
+    {
+      recipeList && recipeList.length > 0 ? 
+      recipeList.map(item=> <RecipeItem item={item} />)
+      : null
+    }
     </div>
   );
 }
